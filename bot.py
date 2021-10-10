6da67b38d5eaf15453b7f9725db7417ae1c6dfad
alan <salan2000@wp.pl>
2021-10-10 15:35:05 +0200

    Finalized last function that executes other functions

--- bot.py
+++ bot.py
@@ -3,8 +3,15 @@ import os
 import urllib.request
 from datetime import date
 
-URL_TO = "http://wt.ajp.edu.pl/images/Plany/II_rok_E-MiBM-I-AiR.pdf"
+#Store current date in variable
 current_date = date.today()
+today_date_header = current_date.strftime("%d %b %Y") #header syntax
+today_date = current_date.strftime("%d-%m-%Y")
+
+URL_TO = "http://wt.ajp.edu.pl/images/Plany/II_rok_E-MiBM-I-AiR.pdf"
+file_name = "Schedule_" + today_date + ".pdf"
+path = "/mnt/For_linux_use/Discord_bots/Downloader_bot/" + file_name
+
 
 client = discord.Client()
 
@@ -23,21 +30,33 @@ def last_modified(url, status):
     else:
         return "Currently page is down"
 
-#Store current date in variable
-today_date = current_date.strftime("%d %b %Y")
+
 
 #Check if header has been updated based on current date
 def verify(response_page, url):
-    if today_date in response_page:
+    if today_date_header in response_page:
         response = urllib.request.urlopen(url)    
         file = open("Schedule_" + today_date + ".pdf", 'wb')
         file.write(response.read())
         file.close()
     else:
-        return "No update"
+        return 
 
+#Login
 @client.event
 async def on_ready(self):
     print('Logged on as {0}!'.format(self.user))
 
+#Execute command
 @client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('-check'):
+        status_page = check_status(URL_TO)
+        header_get = last_modified(URL_TO, status_page)
+        verify(header_get, URL_TO)
+        await message.channel.send(file=discord.File(path))
+
+client.run(os.getenv('DISCORD_TOKEN'))
+        
