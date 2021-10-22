e0bf4ecbd5eb67fa1070c4de789ffd03a487bdfc
alan <salan2000@wp.pl>
2021-10-22 17:06:59 +0200

    Add: added conditionals

--- cogs/cmd_error_handler.py
+++ cogs/cmd_error_handler.py
@@ -9,10 +9,19 @@ class ErrorHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
 
+        if hasattr(ctx.command, "on_error"):
+            return 
+        error = getattr(error, "original", error)
+
         if isinstance(error, commands.CommandNotFound):
             await ctx.send('Beep Beep! Command not found ¯\_(ツ)_/¯ ')
+
         if isinstance(error, commands.UserInputError):
             await ctx.send('Beep Beep! Your input is wrong ¯\_(ツ)_/¯ ')
+
+        if isinstance(error, commands.CommandError):
+            return await ctx.send(f'Command error **{ctx.command.name}** -> {str(error)}')
+            
         else:
             print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
